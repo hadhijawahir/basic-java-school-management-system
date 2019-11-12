94e53236104ed0785208743d0bb56b19ffd9adf6
hadhijawahir <hadhi@live.com>
2019-11-12 20:35:18 +0530

    Student Class Update

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -8,6 +8,16 @@ public class Student {
     private int feesPaid;
     private int feesTotal;
 
+    public Student(int id, String name, int grade){
+        this.feesPaid=0;
+        this.feesTotal=30000;
+        this.id = id;
+        this.name = name;
+        this.grade = grade;
+    }
 
+    public void setGrade{
+
+    }
 
 }
