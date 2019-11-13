9f17c46ce8bccf6142cfb1318344925a535ee097
hadhijawahir <hadhi@live.com>
2019-11-13 12:02:33 +0530

    Student Class Update

--- src/school/management/system/Student.java
+++ src/school/management/system/Student.java
@@ -8,7 +8,7 @@ public class Student {
     private int feesPaid;
     private int feesTotal;
 
-    public Student(int id, String name, int grade){
+    public Student (int id, String name, int grade) {
         this.feesPaid=0;
         this.feesTotal=30000;
         this.id = id;
@@ -16,8 +16,12 @@ public class Student {
         this.grade = grade;
     }
 
-    public void setGrade{
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
 
+    public void updateFeesPaid(int fees) {
+        feesPaid += fees;
     }
 
 }
