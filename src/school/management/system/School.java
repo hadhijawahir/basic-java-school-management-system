28a725a7d4c6a0c8a2cdfb52dd82a2ccacd75b38
hadhijawahir <hadhi@live.com>
2019-11-15 19:31:55 +0530

    School Class Added

--- src/school/management/system/School.java
+++ src/school/management/system/School.java
@@ -2,4 +2,9 @@ package school.management.system;
 
 public class School {
 
+    private Teacher teacher;
+    private Student student;
+    private int totalMoneyEarned;
+    private int totalMoneySpent;
+
 }
